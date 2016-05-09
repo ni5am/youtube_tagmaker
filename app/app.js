534873fa5a8272205b571474f90d3459fb83377f
nisam <nisam0301@gmail.com>
2016-05-09 10:39:47 +0900

    init

---
+++ app/app.js
@@ -0,0 +1,62 @@
+var app = angular.module('app', ['ngStorage']);
+
+
+app.controller('presetCtrl', ['$scope', '$localStorage', '$rootScope', function ($scope, $localStorage, $rootScope) {
+    $scope.presets = $localStorage.presets;
+
+    $scope.select = function(index) {
+
+        if($localStorage.activePreset != null)
+            $scope.presets[$localStorage.activePreset].active = "";
+
+        $scope.presets[index].active = "active";
+
+        $localStorage.activePreset = index;
+
+        $rootScope.$broadcast('update', {});
+
+    };
+
+    $scope.remove = function(index) {
+        if($scope.presets.length == 1){
+            alert('삭제 불가');
+            return;
+        }
+
+        if(index < ($scope.presets.length-1)){
+            $scope.presets[index+1].active = "active";
+        }else {
+            $scope.presets[index-1].active = "active";
+            $localStorage.activePreset -= 1;
+        }
+
+        $scope.presets.splice(index, 1);
+
+        $rootScope.$broadcast('update', {});
+
+    };
+
+    $scope.add = function() {
+        var preset = {"name":"Untitled Profile", "active":"active"}; // 디폴트 세팅이 여기에 박힐거얌
+
+        if($scope.presets == null){
+            $scope.presets = [];
+        }
+        $scope.presets.push(preset);
+
+        $localStorage.presets = $scope.presets;
+
+        $scope.select($scope.presets.length-1);
+
+        $rootScope.$broadcast('update', {});
+
+    }
+}]);
+
+app.controller('controllerCtrl', ['$scope', '$localStorage', function ($scope, $localStorage) {
+    $scope.preset = $localStorage.presets[$localStorage.activePreset];
+
+    $scope.$on('update', function() {
+        $scope.preset = $localStorage.presets[$localStorage.activePreset];
+    });
+}]);
--- js/background.js
+++ js/background.js
@@ -1,1 +1,10 @@
-//유튜브 사이트에 들어오면 아이콘이 빛나게하는 백그라운드 기능 수행
+chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        console.log(url);
+
+        //이게 유튜브면 활성화~ 아이콘 색변경하기
+        //
+    });
+});
+
